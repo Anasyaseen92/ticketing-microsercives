694206ae15d584e2daed183cef826357cba01390
Anas Yaseen <anasy7148@gmail.com>
2026-06-15 04:22:26 +0500

    Working on SSR logics

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -14,9 +14,22 @@ const LandingPage = ({currentUser}: LandingPageProps) => {
   return <h1>Landing Page</h1>;
 };
 
-/*LandingPage.getInitialProps = () => {
-  console.log("i am on the server");
-  return { currentUser: null };
-};*/
+LandingPage.getInitialProps = async () => {
+if(typeof window === "undefined") {
+  const {data} = await axios.get('http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser',{
+    headers: {
+      Host: "ticketing.dev"
+    }
+  });
+
+  return data;
+}
+else {
+
+  const {data} = await axios.get("/api/users/currentuser");
+  return data;
+}
+
+};
 
 export default LandingPage;
