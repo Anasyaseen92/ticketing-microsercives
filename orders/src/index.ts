3aa859d8d26d22d5bcdff47affa44fd1f2c82e3d
Anas Yaseen <anasy7148@gmail.com>
2026-07-15 23:45:11 +0500

    DOne

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import {TicketCreatedListener} from './events/listeners/ticket-created-listener';
+import {TicketUpdatedListener} from './events/listeners/ticket-updated-listener';
+
 const start = async () => {
   if(!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
@@ -14,16 +17,18 @@ const start = async () => {
     console.log('NATS connection closed!');
     process.exit();
   });
-  process.on('SIGINT', () => natsWrapper.client?.close());
+process.on('SIGINT', () => natsWrapper.client?.close());
 process.on('SIGTERM', () => natsWrapper.client?.close());
 
-    await mongoose.connect(process.env.MONGO_URI);
+new TicketCreatedListener(natsWrapper.client).listen();
+new TicketUpdatedListener(natsWrapper.client).listen();
+
+    await mongoose.connect(process.env.MONGO_URI)
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error('Failed to connect to MongoDB', err);
   }
 app.listen(3000, () => {
-  
   console.log('Auth service is running on port 3000...');
 });
 }
